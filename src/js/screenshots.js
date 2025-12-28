3f106e4171f80c6eb23890f7d2b52c7da161127b
YevhenKharchenko <evgenvalerych@gmail.com>
2025-12-28 09:46:41 +0200

    Initial commit

---
+++ src/js/screenshots.js
@@ -0,0 +1,69 @@
+import Swiper from 'swiper';
+import 'swiper/css/bundle';
+
+const screenshotsLeftArrow = document.getElementById('screenshotsLeftArrow');
+const screenshotsRightArrow = document.getElementById('screenshotsRightArrow');
+const screenshotsDots = document.querySelectorAll('.screenshots-dot');
+
+let screenshotsSwiper;
+
+screenshotsSwiper = new Swiper('.screenshots-swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  centeredSlides: true,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  initialSlide: 0,
+  spaceBetween: 12,
+  speed: 500,
+  allowTouchMove: true,
+  grabCursor: true,
+  breakpoints: {
+    1440: {
+      slidesPerView: 'auto',
+      initialSlide: 2,
+      spaceBetween: 24,
+    },
+  },
+
+  on: {
+    init(swiper) {
+      document
+        .querySelector('.screenshots-swiper-container')
+        .classList.add('show');
+      updateScreenshotsArrows(swiper);
+      updateScreenshotsDots(swiper.activeIndex);
+    },
+    slideChange(swiper) {
+      updateScreenshotsArrows(swiper);
+      updateScreenshotsDots(swiper.activeIndex);
+    },
+  },
+});
+
+updateScreenshotsArrows(screenshotsSwiper);
+
+function updateScreenshotsArrows(swiper) {
+  screenshotsLeftArrow.disabled = swiper.isBeginning;
+  screenshotsRightArrow.disabled = swiper.isEnd;
+}
+
+screenshotsLeftArrow.addEventListener('click', () => {
+  screenshotsSwiper.slidePrev();
+});
+
+screenshotsRightArrow.addEventListener('click', () => {
+  screenshotsSwiper.slideNext();
+});
+
+function updateScreenshotsDots(index) {
+  screenshotsDots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
+}
+
+screenshotsDots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    screenshotsSwiper.slideTo(index);
+  });
+});
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './js/header.js';
+import './js/screenshots.js';
+import './js/faq.js';
+import './js/banner.js';
